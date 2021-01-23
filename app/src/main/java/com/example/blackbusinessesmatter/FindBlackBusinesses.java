8f24b0d3370ada7c34c106bde22375656febbaa1
Adam Jacobs <adam.jacobs.100@gmail.com>
2021-01-23 00:27:10 +0000

    home page made with on click listner and first recyler view layout in search bar added

---
+++ app/src/main/java/com/example/blackbusinessesmatter/FindBlackBusinesses.java
@@ -0,0 +1,14 @@
+package com.example.blackbusinessesmatter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindBlackBusinesses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_black_businesses);
+    }
+}
