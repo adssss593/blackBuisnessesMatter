160fa87e35f22c88f4def2efcef0a0bfd6f939d0
Adam Jacobs <adam.jacobs.100@gmail.com>
2021-02-04 15:48:42 +0000

    recyler views added and individual business views for each business added

---
+++ app/src/main/java/com/example/blackbusinessesmatter/Utils.java
@@ -0,0 +1,4 @@
+package com.example.blackbusinessesmatter;
+
+public class Utils {
+}
---
+++ app/src/main/java/com/example/blackbusinessesmatter/whatIsThis.java
@@ -0,0 +1,14 @@
+package com.example.blackbusinessesmatter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class whatIsThis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_what_is_this);
+    }
+}
